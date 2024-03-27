143b8e259251103165e45e2996eb8b6d61f3ee3a
karthikakrishnan14 <karthikakrishnan14@gmail.com>
2024-03-27 12:15:11 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,8 @@ def generate_response(txt):
     return chain.run(docs)
 
 # Page title
-st.set_page_config(page_title='🦜🔗 Text Summarization App')
-st.title('🦜🔗 Text Summarization App')
+st.set_page_config(page_title='Text Summarization App')
+st.title('Summarize your Text')
 
 # Text input
 txt_input = st.text_area('Enter your text', '', height=200)
